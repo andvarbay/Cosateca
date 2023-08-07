f7ab58bb477069912d73d88756cd5e79b56b2d1a
andvarbay <avargasb01@gmail.com>
2023-08-07 16:51:26 +0200

    feat: creación de .gitignore y entidad usuario
    Creación de .gitignore, creación de entidad usuario y vista inicio

--- CosatecaApp/models.py
+++ CosatecaApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+
+    nombre = models.CharField(blank=True, null=True, max_length=20)
+    apellidos = models.CharField(blank=True, null=True, max_length=50)
+    correo = models.CharField(unique=True, max_length=80)
+    contrasenha = models.CharField(max_length=80)
+    nombreusuario = models.CharField(db_column='nombreUsuario', unique=True, max_length=20)  # Field name made lowercase.
+    ubicacion = models.CharField(blank=True, null=True, max_length=20)
+    rol = models.CharField(blank=True, null=True, max_length=13, choices=(('administrador','administrador'), ('usuario', 'usuario')))
+
+    class Meta:
+        managed = False
+        db_table = 'usuario'
