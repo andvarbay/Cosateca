10018ddbae2cfeeb6f879017b93485c5f3110407
andvarbay <avargasb01@gmail.com>
2023-08-09 18:08:48 +0200

    feat: creación de entidades
    Creación de todas las entidades de Cosateca y adición de estas a admin.

--- CosatecaApp/admin.py
+++ CosatecaApp/admin.py
@@ -3,4 +3,16 @@ from CosatecaApp.models import *
 
 # Register your models here.
 
+admin.site.register(Categoria)
+admin.site.register(Chat)
+admin.site.register(Estadistica)
+admin.site.register(EstadisticaUsuario)
+admin.site.register(Logro)
+admin.site.register(LogroUsuario)
+admin.site.register(Mensaje)
+admin.site.register(Notificacion)
+admin.site.register(Prestamo)
+admin.site.register(Producto)
+admin.site.register(Reporte)
 admin.site.register(Usuario)
+admin.site.register(Valoracion)
