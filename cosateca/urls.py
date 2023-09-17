9f9e07307b2a1a5581cef780601b82297e0f67b5
andvarbay <avargasb01@gmail.com>
2023-09-17 19:12:56 +0200

    Fix: arreglo conflicto merge en urls.py

--- cosateca/urls.py
+++ cosateca/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     path('listado-chats/', views.listadoChats, name='listadoChats'),
     path('editarPerfil/<idUsuario>', EditarPerfil.as_view(), name='editarPerfil'),
     path('detallesProducto/<idProducto>',views.detallesProducto, name="detalles"),
-    path('registroPrestamos', views.registroPrestamos, name='registroPrestamos')
+    path('registroPrestamos', views.registroPrestamos, name='registroPrestamos'),
     path('valorarProducto/',FormularioValoración.as_view(), name='valorarProducto'),
 ]
